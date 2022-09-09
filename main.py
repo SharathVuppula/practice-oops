4151ab5924396d243f61798cbd09880987396535
SharathVuppula <sharathvuppala05@gmail.com>
2022-09-09 07:50:37 +0530

    created class methods  to  display  and  change tires

--- main.py
+++ main.py
@@ -12,6 +12,23 @@ class car :
         print ('max speed is: ', self.max_speed)
         print ('mileage  is: ',  self.mileage)
 
+# creating a class method named display tires
+    @classmethod
+    def displaytires(cls):
+        print ("Every  car has", car.tires, "tires")
+
+    @classmethod
+    def changenumbeoftires(cls, tires):
+        cls.tires =  tires
+        print ("Number of tires is changed to ", car.tires)
+
+# global method
+def speeddiff(a:car , b:car):
+    if(type(a) != type(b)):
+        print('cannot compare')
+        return
+    print('Difference in speed is:', a.max_speed - b.max_speed)
+
 class bike :
     def __init__(self, max_speed) :
         self.max_speed = max_speed
@@ -24,11 +41,6 @@ shine = bike(90)
 # altroz.prntdetails()
 # print(type(car))
 
-def speeddiff(a:car , b:car):
-    if(type(a) != type(b)):
-        print('cannot compare')
-        return
-    print('Difference in speed is:', a.max_speed - b.max_speed)
 
 speeddiff(nexon, altroz)
 speeddiff(nexon,  shine)
@@ -38,3 +50,6 @@ print (altroz.tires)
 altroz.tires = 7
 print (nexon.tires)
 print (altroz.tires)
+
+car.displaytires()
+car.changenumbeoftires(8)
